6b653de3ea72ac8b5186ca0fa526a626d125678f
Karina546654 <91517120+Karina546654@users.noreply.github.com>
2023-04-20 15:29:01 +0500

    Add files via upload

---
+++ WpfApp1 — копия/WpfApp1/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class Book
+    {
+        public string Name { get; set; }
+        public string Year { get; set; }
+        public string Author { get; set; }
+
+
+        public Book (string name, string year, string auth)
+        {
+            Name = name;
+            Year = year;
+            Author = auth;
+        }
+    }
+}
---
+++ WpfApp1 — копия/WpfApp1/BookViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Windows.Input;
+using System.Reflection.Metadata;
+
+namespace WpfApp1
+{
+    internal class BookViewModel
+    {
+        public ObservableCollection<Book> Books { get; set; }
+
+        private string _jsonFile = "book.json";
+
+        public ICommand AddUsersCommand { get; set; }
+       
+        public BookViewModel()
+        {
+            Books = new ObservableCollection<Book>();
+            LoadBooks();
+
+            AddUsersCommand = new RelayCommand(
+                (parameter) =>
+                {
+                    var bookRegistration = parameter as MainWindow;
+                    string name = bookRegistration.NameTB.Text;
+                    string year = bookRegistration.YearTB.Text;
+                    string auth = bookRegistration.authTB.Text;
+                    if (!string.IsNullOrEmpty(year) && !string.IsNullOrEmpty(name) && !string.IsNullOrEmpty(auth))
+                    {
+                        AddBook(name, auth, year);
+                        bookRegistration.NameTB.Clear();
+                        bookRegistration.YearTB.Clear();
+                        bookRegistration.authTB.Clear();
+
+                    }
+                });
+        }
+
+        private void SaveBooks()
+        {
+            var json = JsonConvert.SerializeObject(Books);
+            File.AppendAllText(_jsonFile, json);
+        }
+        private void LoadBooks()
+        {
+            if (File.Exists(_jsonFile))
+            {
+                var json = File.ReadAllText(_jsonFile);
+                var bookList = JsonConvert.DeserializeObject<ObservableCollection<Book>>(json);
+                Books = bookList;
+            }
+         
+        }
+        private void AddBook(string name, string year, string auth)
+        {
+            Books.Add(new Book(name, year, year));
+            LoadBooks();
+        }
+
+    }
+}

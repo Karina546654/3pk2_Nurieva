eed89f8ced522e04e11d0a1f1a2d919305e1eb11
Karina546654 <91517120+Karina546654@users.noreply.github.com>
2023-04-20 15:29:45 +0500

    Add files via upload

---
+++ WpfApp1/WpfApp1/RelayCommand.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp1
+{
+    internal class RelayCommand: ICommand
+    {
+        private readonly Predicate<object> _canExecute;
+        private readonly Action<object> _execute;
+
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute.Invoke(parameter);
+        }
+        public void Execute(object? parameter)
+        {
+            _execute?.Invoke(parameter);
+        }
+
+        public event EventHandler? CanExecuteChanged
+        {
+           add
+            {
+                if (CanExecute != null)
+                    CommandManager.RequerySuggested += value;
+
+            }
+            remove
+            {
+                if (CanExecute != null)
+                { CommandManager.RequerySuggested -= value; }
+            }
+        }
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute = null)
+        {
+            _canExecute = canExecute;
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/UserViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Windows.Input;
+using System.Reflection.Metadata;
+
+namespace WpfApp1
+{
+    internal class UserViewModel
+    {
+        public ObservableCollection<User> Users { get; set; }
+
+        private string _jsonFile = "users.json";
+
+        public ICommand AddUsersCommand { get; set; }
+       
+        public UserViewModel()
+        {
+            Users = new ObservableCollection<User>();
+            LoadUsers();
+
+            AddUsersCommand = new RelayCommand(
+                (parameter) =>
+                {
+                    var userRegistration = parameter as MainWindow;
+                    string login = userRegistration.LogTB.Text;
+                    string password = userRegistration.pass.Password;
+                    string email = userRegistration.emailTB.Text;
+                    if (!string.IsNullOrEmpty(email) && !string.IsNullOrEmpty(login) && !string.IsNullOrEmpty(password))
+                    {
+                        AddUser(login, password, email);
+                        userRegistration.LogTB.Clear();
+                        userRegistration.pass.Clear();
+                        userRegistration.emailTB.Clear();
+
+                    }
+                });
+        }
+
+        private void SaveUsers()
+        {
+            var json = JsonConvert.SerializeObject(Users);
+            File.AppendAllText(_jsonFile, json);
+        }
+        private void LoadUsers()
+        {
+            if (File.Exists(_jsonFile))
+            {
+                var json = File.ReadAllText(_jsonFile);
+                var userList = JsonConvert.DeserializeObject<ObservableCollection<User>>(json);
+                Users = userList;
+            }
+         
+        }
+        private void AddUser(string login, string pass, string email)
+        {
+            Users.Add(new User(login, pass, email));
+            LoadUsers();
+        }
+
+    }
+}

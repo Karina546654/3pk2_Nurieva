76ee3378735bc1d3521e40a75005910cfe9e77b8
Karina546654 <91517120+Karina546654@users.noreply.github.com>
2023-05-26 09:55:43 +0500

    Add files via upload

---
+++ pz_8/pz_8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace pz_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                IContact person = ContactFactory.CreateContact("person");
+                person.Name = "Иванов Иван";
+                person.Phone = "89878288280";
+                person.Email = "ivan@mail.ru";
+                person.Address = "Чкалова, 7";
+                ((Person)person).Birthday = "10.10.2004";
+                person.Display();
+            }
+        }
+    }
+}
